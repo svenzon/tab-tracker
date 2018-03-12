872237779e2527229864e01521051df5615286fb
svenzon <anders.m.sv@gmail.com>
2018-03-12 15:09:43 +0100

    Song display styled (somewhat)

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -14,10 +14,38 @@
                     @click="navigateTo({name: 'songs-create'})">
                         <v-icon>add</v-icon>
                 </v-btn>
-                <div v-for="song in songs" :key="song.id">
-                    {{song.title}} -
-                    {{song.artist}} -
-                    {{song.album}}
+                <div v-for="song in songs"
+                    class="song"
+                    :key="song.id">
+
+                    <v-layout>
+                        <v-flex xs6>
+                            <div class="song-title">
+                                {{song.title}}
+                            </div>
+                            <div class="song-artist">
+                                {{song.artist}}
+                            </div>
+                            <div class="song-genre">
+                                {{song.genre}}
+                            </div>
+
+                            <v-btn
+                                dark
+                                class="cyan"
+                                @click="navigateTo(
+                                    {
+                                        name: 'song',
+                                        params: {songId: song.id}
+                                    }
+                                )">
+                                View Song
+                            </v-btn>
+                        </v-flex>
+                        <v-flex xs6>
+                            <img class="album-image" :src="song.albumImageUrl">
+                        </v-flex>
+                    </v-layout>
                 </div>
             </panel>
         </v-flex>
@@ -51,4 +79,25 @@ export default {
 
 <style scoped>
 
+    .song {
+        padding: 20px;
+        height: 330px;
+        overflow: hidden;
+    }
+
+    .song-title {
+        font-size: 30px;
+    }
+
+    .song-artist {
+        font-size: 24px;
+    }
+
+    .song-genre {
+        font-size: 18px;
+    }
+
+    .album-image {
+        width: 70%;
+    }
 </style>
---
+++ client/src/components/ViewSong.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>Placeholder</p>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+
+</style>
