297526ff49d04a8cd64f64f4813d718a7d04843d
svenzon <anders.m.sv@gmail.com>
2018-03-14 09:46:01 +0100

    Separate SongPanel component created

--- client/src/components/Songs/SongsIndex.vue
+++ client/src/components/Songs/SongsIndex.vue
@@ -1,102 +1,21 @@
 <template>
     <v-layout column class="mt-4">
         <v-flex xs6 offset-xs3>
-            <panel title="Songs">
-                <v-btn
-                    class="cyan accent-2"
-                    light
-                    small
-                    absolute
-                    right
-                    middle
-                    fab
-                    slot="action"
-                    @click="navigateTo({name: 'songs-create'})">
-                        <v-icon>add</v-icon>
-                </v-btn>
-                <div v-for="song in songs"
-                    class="song"
-                    :key="song.id">
-
-                    <v-layout>
-                        <v-flex xs6>
-                            <div class="song-title">
-                                {{song.title}}
-                            </div>
-                            <div class="song-artist">
-                                {{song.artist}}
-                            </div>
-                            <div class="song-genre">
-                                {{song.genre}}
-                            </div>
-
-                            <v-btn
-                                dark
-                                class="cyan"
-                                @click="navigateTo(
-                                    {
-                                        name: 'song',
-                                        params: {songId: song.id}
-                                    }
-                                )">
-                                View Song
-                            </v-btn>
-                        </v-flex>
-                        <v-flex xs6>
-                            <img class="album-image" :src="song.albumImageUrl">
-                        </v-flex>
-                    </v-layout>
-                </div>
-            </panel>
+            <songs-panel/>
         </v-flex>
     </v-layout>
 </template>
 
 <script>
-import Panel from '@/components/Panel'
-import SongsService from '@/services/SongsService'
+import SongsPanel from './SongsPanel'
 
 export default {
-    data () {
-        return {
-            songs: null
-        }
-    },
     components: {
-        Panel
-    },
-    methods: {
-        navigateTo (route) {
-            this.$router.push(route)
-        }
-    },
-    async mounted () {
-        // find the songs in the backend
-        this.songs = (await SongsService.index()).data
+        SongsPanel
     }
 }
 </script>
 
 <style scoped>
-.song {
-    padding: 20px;
-    height: 330px;
-    overflow: hidden;
-}
 
-.song-title {
-    font-size: 30px;
-}
-
-.song-artist {
-    font-size: 24px;
-}
-
-.song-genre {
-    font-size: 18px;
-}
-
-.album-image {
-    width: 70%;
-}
 </style>
